c4e2225c2577d2baaabf216883fc170e27b51cc1
Hle14 <HansLe518@gmail.com>
2014-04-28 21:03:30 -0400

    fixed clock and html-formatters

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@ var hourly_wage = earned = 0;
 var setHourlyWage = function(){
 	//NOTE: this won't check to make sure the wage is actually a number
 	hourly_wage = document.getElementById("wage_input").value;
-	document.getElementById("wage").innerHTML = "$" + hourly_wage;
+	document.getElementById("hourly_wage").innerHTML = hourly_wage;
 };
 
 var addToWage = function(){
 	earned += parseFloat(hourly_wage);
-	document.getElementById("earned").innerHTML = "$" + earned;
+	document.getElementById("earned").innerHTML = earned;
 }
 /*
 var setStartTime = function(){
@@ -22,10 +22,10 @@ var updateClockAndWage = function(){
 	var time = new Date();
 
 	//get current time in hours minutes seconds
-	var hour = time.detHours();
+	var hour = time.getHours();
 	var minute = time.getMinutes();
 	var second = time.getSeconds();
-/*
+
 	//pad minute/second with zeros if appropriate
 	minute = (minute < 10 ? "0" : "") + minute;
 	second = (second < 10 ? "0" : "") + second;
@@ -43,17 +43,39 @@ var updateClockAndWage = function(){
 	//update clock display
 	//document.getElementById("clock").firstChild.nodeValue = time_string;
 	document.getElementById("clock").innerHTML = time_string;
-	//update wages earned display
-	document.getElementById("wage").innerHTML = earned_string;
-	*/
-	document.getElementById("clock").innerHTML = time;
-}
+};
+
+//global declaration for access by start and clear timer methods
+var wage_counter,clock_counter;
+var clearTimers = function(){
+	clearInterval(wage_counter);
+	clearInterval(clock_counter);
+};
+var startTimers = function(){
+	clearTimers();
+	wage_counter = setInterval(addToWage,500);
+	clock_counter = setInterval(updateClockAndWage,1000);
+};
+var reset = function(){
+	clearTimers();
+	hourly_wage = earned = 0;
+	document.getElementById("wage_input").value = 0;
+	document.getElementById("earned").innerHTML = 0;
+	document.getElementById('hourly_wage').innerHTML = 0;
+};
 
 window.onload = function(){
 
+	//set up event listeners
 	var setWage = document.getElementById("set_wage");
 	setWage.addEventListener("click",setHourlyWage,false);
 
-	setInterval(function(){addToWage()},500);
-	//setInterval(function(){updateClockAndWage()},1000);
+	var stopButton = document.getElementById("stop_button");
+	stopButton.addEventListener("click",clearTimers,false);
+
+	var startButton = document.getElementById("start_button");
+	startButton.addEventListener("click",startTimers,false);
+
+	var clearButton = document.getElementById("clear_button");
+	clearButton.addEventListener("click",reset,false);
 };
