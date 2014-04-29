cec5b8d5bb3e31d525d454f7a3b3069c1093ef64
Hle14 <HansLe518@gmail.com>
2014-04-28 20:07:45 -0400

    added money earned ticker

--- scripts.js
+++ scripts.js
@@ -1,26 +1,31 @@
-var hourly_wage = 0;
-var setWage = document.getElementById("set_wage");
 
-setWage.addEventListener("click",setHourlyWage,false);
+var hourly_wage = earned = 0;
+//var form = document.getElementById("wage_form");
 
-function setHourlyWage(){
+var setHourlyWage = function(){
 	//NOTE: this won't check to make sure the wage is actually a number
 	hourly_wage = document.getElementById("wage_input").value;
+	document.getElementById("wage").innerHTML = "$" + hourly_wage;
+};
+
+var addToWage = function(){
+	earned += parseFloat(hourly_wage);
+	document.getElementById("earned").innerHTML = "$" + earned;
 }
+/*
+var setStartTime = function(){
 
-function setStartTime(){
+};
+*/
 
-}
-
-function updateClockAndWage(){
-
+var updateClockAndWage = function(){
 	var time = new Date();
 
 	//get current time in hours minutes seconds
 	var hour = time.detHours();
 	var minute = time.getMinutes();
 	var second = time.getSeconds();
-
+/*
 	//pad minute/second with zeros if appropriate
 	minute = (minute < 10 ? "0" : "") + minute;
 	second = (second < 10 ? "0" : "") + second;
@@ -36,11 +41,19 @@ function updateClockAndWage(){
 	var earned = hourly_wage*(hour + (minute*60 + second)/3600);
 	var earned_string = "$" + earned.toFixed(2);
 	//update clock display
-	document.getElementById("clock").firstChild.nodeValue = time_string;
+	//document.getElementById("clock").firstChild.nodeValue = time_string;
+	document.getElementById("clock").innerHTML = time_string;
 	//update wages earned display
-	document.getElementById("wages").firstChild.nodeValue = earned_string;
+	document.getElementById("wage").innerHTML = earned_string;
+	*/
+	document.getElementById("clock").innerHTML = time;
 }
 
 window.onload = function(){
-	setInterval('updateClockAndWage',1000);
+
+	var setWage = document.getElementById("set_wage");
+	setWage.addEventListener("click",setHourlyWage,false);
+
+	setInterval(function(){addToWage()},500);
+	//setInterval(function(){updateClockAndWage()},1000);
 };
